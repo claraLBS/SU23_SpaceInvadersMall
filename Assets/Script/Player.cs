711c37132f858896c34f3abbe193bdd068ea51af
claraLBS <clara.witty@lbs.se>
2024-10-08 11:28:56 +0200

    Initial commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    public Laser laserPrefab;
+    Laser laser;
+    float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) && laser == null)
+        {
+            laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Missile") || collision.gameObject.layer == LayerMask.NameToLayer("Invader"))
+        {
+            GameManager.Instance.OnPlayerKilled(this);
+        }
+    }
+}
